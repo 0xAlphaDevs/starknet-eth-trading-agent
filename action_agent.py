90a853515b1a379bbb21d4aef06637398e3e1b2a
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-06-11 10:38:11 +0530

    chore: add requirements and lock file

--- action_agent.py
+++ action_agent.py
@@ -4,5 +4,9 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 dev_private_key = os.environ.get("PRIVATE_KEY")
+starknet_rpc_url = os.environ.get("STARKNET_RPC_URL")
+dry_run = os.environ.get("DRY_RUN")
 
 print("private key is: ", dev_private_key)
+print("starknet rpc url is: ", starknet_rpc_url)
+print("dry run is: ", dry_run)
